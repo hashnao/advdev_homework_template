e890d00c617251d5503e1cc3574629d19d684b36
Naoya Hashimoto <nhashimo@redhat.com>
2018-12-02 17:20:26 +0900

    Modified environment variables for nexus, sonar, and registry

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -15,7 +15,11 @@ MAVEN_SLAVE_IMAGE=jenkins-slave-appdev
 MAVEN_PATH=/opt/rh/rh-maven35/root/usr/bin
 GIT_SOURCE_REF=master
 NEXUS_URL="http://nexus.${GUID}-nexus.svc:8081/repository/maven-releases/"
-SONAR_URL="http://sonarqube.${GUID}-sonarqube.svc:9000/"
+NEXUS_REGISTRY_URL="docker://nexus-registry.${GUID}-nexus.svc:5000"
+NEXUS_USER=admin
+NEXUS_PASSWORD=admin123
+SONAR_URL="http://sonarqube.${GUID}-sonarqube.svc:9000"
+REGISTRY_URL="docker://nexus-registry.${GUID}-nexus.svc:5000"
 
 # Create custom agent container image with skopeo
 oc project ${GUID}-jenkins
@@ -43,7 +47,11 @@ oc new-build ${REPO} --strategy=pipeline --context-dir=${CONTEXT_DIR} --name=${A
 -e APP_IMAGE=${APP_IMAGE} \
 -e MAVEN_PATH=${MAVEN_PATH} \
 -e NEXUS_URL=${NEXUS_URL} \
--e SONAR_URL=${SONAR_URL}
+-e NEXUS_REGISTRY_URL=${NEXUS_REGISTRY_URL} \
+-e NEXUS_USER=${NEXUS_USER} \
+-e NEXUS_PASSWORD=${NEXUS_PASSWORD} \
+-e SONAR_URL=${SONAR_URL} \
+-e REGISTRY_URL=${REGISTRY_URL}
 
 CONTEXT_DIR=Nationalparks
 APP_NAME=$(echo ${CONTEXT_DIR} | tr '[:upper:]' '[:lower:]')
@@ -58,7 +66,11 @@ oc new-build ${REPO} --strategy=pipeline --context-dir=${CONTEXT_DIR} --name=${A
 -e APP_IMAGE=${APP_IMAGE} \
 -e MAVEN_PATH=${MAVEN_PATH} \
 -e NEXUS_URL=${NEXUS_URL} \
--e SONAR_URL=${SONAR_URL}
+-e NEXUS_REGISTRY_URL=${NEXUS_REGISTRY_URL} \
+-e NEXUS_USER=${NEXUS_USER} \
+-e NEXUS_PASSWORD=${NEXUS_PASSWORD} \
+-e SONAR_URL=${SONAR_URL} \
+-e REGISTRY_URL=${REGISTRY_URL}
 
 CONTEXT_DIR=ParksMap
 APP_NAME=$(echo ${CONTEXT_DIR} | tr '[:upper:]' '[:lower:]')
@@ -73,4 +85,8 @@ oc new-build ${REPO} --strategy=pipeline --context-dir=${CONTEXT_DIR} --name=${A
 -e APP_IMAGE=${APP_IMAGE} \
 -e MAVEN_PATH=${MAVEN_PATH} \
 -e NEXUS_URL=${NEXUS_URL} \
--e SONAR_URL=${SONAR_URL}
+-e NEXUS_REGISTRY_URL=${NEXUS_REGISTRY_URL} \
+-e NEXUS_USER=${NEXUS_USER} \
+-e NEXUS_PASSWORD=${NEXUS_PASSWORD} \
+-e SONAR_URL=${SONAR_URL} \
+-e REGISTRY_URL=${REGISTRY_URL}
