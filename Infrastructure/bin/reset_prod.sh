9789b296e4ddf1d9d2eaacb3cc63e268f06f6d7c
Naoya Hashimoto <nhashimo@redhat.com>
2019-02-03 19:23:57 +0900

    Changing service backend label

--- Infrastructure/bin/reset_prod.sh
+++ Infrastructure/bin/reset_prod.sh
@@ -15,6 +15,13 @@ source ${BIN_PATH:-./Infrastructure/bin}/utils.sh
 echo "--- Resetting Parks Production Environment in project ${NAMESPACE_PROD} to Green Services. ---"
 
 oc project ${NAMESPACE_PROD}
+echo "--- Setting pod replica for green application with 0 and blue with 1."
+ALL_APPLICATIONS="mlbparks nationalparks parksmap"
+for i in ${ALL_APPLICATIONS} ; do
+  oc scale dc ${i}-blue --replicas=0
+  oc scale dc ${i}-green --replicas=1
+  oc rollout status dc ${i}-green -w
+done
 # For backend services
 BACKEND_APPLICATIONS="mlbparks nationalparks"
 for i in ${BACKEND_APPLICATIONS} ; do
