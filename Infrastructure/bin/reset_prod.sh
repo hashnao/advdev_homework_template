f7a470b8e309e65cd4f98ac32f07f97e76d2a307
Naoya Hashimoto <nhashimo@redhat.com>
2019-02-04 09:44:41 +0900

    Switch all route backend from blue to green

--- Infrastructure/bin/reset_prod.sh
+++ Infrastructure/bin/reset_prod.sh
@@ -15,20 +15,23 @@ source ${BIN_PATH:-./Infrastructure/bin}/utils.sh
 echo "--- Resetting Parks Production Environment in project ${NAMESPACE_PROD} to Green Services. ---"
 
 oc project ${NAMESPACE_PROD}
-echo "--- Setting pod replica for green application with 0 and blue with 1."
+
+echo "--- Scaling down blue pod replica to 0 and out green pod to 1. ---"
 ALL_APPLICATIONS="mlbparks nationalparks parksmap"
 for i in ${ALL_APPLICATIONS} ; do
   oc scale dc ${i}-blue --replicas=0
   oc scale dc ${i}-green --replicas=1
   oc rollout status dc ${i}-green -w
 done
-# For backend services
+
+echo "--- Setting the backend service label on only the green service. ---"
 BACKEND_APPLICATIONS="mlbparks nationalparks"
 for i in ${BACKEND_APPLICATIONS} ; do
   oc label service ${i}-blue type="" --overwrite
   oc label service ${i}-green type=${BACKEND_SERVICE} --overwrite
 done
 
-# For route
-_APP_NAME=parksmap
-oc set route-backends ${_APP_NAME} ${_APP_NAME}-blue=0 ${_APP_NAME}-green=100
+echo "--- Setting weight of green service for route. ---"
+for i in ${ALL_APPLICATIONS} ; do
+  oc set route-backends ${i} ${i}-blue=0 ${i}-green=100
+done
